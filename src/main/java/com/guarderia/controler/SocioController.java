85382bd2c33d57d1c031671b3ca279d581eecf2d
Leandro Fulvio <pika.lean@gmail.com>
2024-08-19 22:12:03 -0300

    added preauth

--- src/main/java/com/guarderia/controler/SocioController.java
+++ src/main/java/com/guarderia/controler/SocioController.java
@@ -4,6 +4,7 @@ import com.guarderia.request.SocioRequest;
 import com.guarderia.service.SocioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,22 +15,26 @@ public class SocioController {
     private final SocioService service;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('admin:read') or hasAuthority('socio:read') or hasAuthority('empleado:read')")
     public ResponseEntity<?> findAllVehicles(){
         return ResponseEntity.ok(service.findAll());
     }
 
     @GetMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('admin:read') or hasAuthority('socio:read') or hasAuthority('empleado:read')")
     public ResponseEntity<?> findById(@PathVariable Long id){
         return ResponseEntity.ok(service.findById(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('admin:create')")
     public ResponseEntity<?> create(@RequestBody SocioRequest request){
         return ResponseEntity.ok(service.save(request));
     }
 
     //update
     @PutMapping(value = "/{id}", produces = "application/json")
+    @PreAuthorize("hasAuthority('admin:update')")
     public ResponseEntity<?> update(@PathVariable Long id,
                                     @RequestBody SocioRequest request){
         return ResponseEntity.ok(service.update(id, request));
@@ -37,6 +42,7 @@ public class SocioController {
 
     //delete
     @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('admin:delete')")
     public ResponseEntity<?> deleteZona(@PathVariable Long id){
         service.deleteById(id);
 
--- src/main/java/com/guarderia/controler/VehiculoController.java
+++ src/main/java/com/guarderia/controler/VehiculoController.java
@@ -4,6 +4,7 @@ import com.guarderia.request.VehiculoRequest;
 import com.guarderia.service.VehiculoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,28 +15,33 @@ public class VehiculoController {
     private final VehiculoService service;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('admin:read') or hasAuthority('socio:read') or hasAuthority('empleado:read')")
     public ResponseEntity<?> findAllVehicles(){
         return ResponseEntity.ok(service.findAll());
     }
 
     @GetMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('admin:read') or hasAuthority('socio:read') or hasAuthority('empleado:read')")
     public ResponseEntity<?> findById(@PathVariable Long id){
         return ResponseEntity.ok(service.findById(id));
     }
 
     @GetMapping(value = "/{id}/socio")
+    @PreAuthorize("hasAuthority('admin:read') or hasAuthority('socio:read') or hasAuthority('empleado:read')")
     public ResponseEntity<?> findBySocioId(@PathVariable Long id){
         return ResponseEntity.ok(service.findBySocioId(id));
     }
 
     //create
     @PostMapping
+    @PreAuthorize("hasAuthority('admin:create')")
     public ResponseEntity<?> create(@RequestBody VehiculoRequest request){
         return ResponseEntity.ok(service.save(request));
     }
 
     //update
     @PutMapping(value = "/{id}", produces = "application/json")
+    @PreAuthorize("hasAuthority('admin:update')")
     public ResponseEntity<?> update(@PathVariable Long id,
                                         @RequestBody VehiculoRequest vehiculo){
         service.update(id, vehiculo);
@@ -47,6 +53,7 @@ public class VehiculoController {
 
     //delete
     @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('admin:delete')")
     public ResponseEntity<?> deleteZona(@PathVariable Long id){
 
         service.deleteById(id);
